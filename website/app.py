55d6f09603e4018842aa9ad2fc6c8e0acbeb2fca
Nguyễn Minh Hoàng <75627141+nguyenhoang2001@users.noreply.github.com>
2024-04-18 23:14:18 +1000

    Set up Dash website structure

---
+++ website/app.py
@@ -0,0 +1,20 @@
+from dash import Dash
+import dash_bootstrap_components as dbc
+import pandas as pd
+from pages.index.index_layout import layout
+
+external_stylesheets = [dbc.themes.CERULEAN]
+
+app = Dash(
+    __name__,
+    use_pages=True,
+    suppress_callback_exceptions=True,
+    external_stylesheets=external_stylesheets,
+)
+
+server = app.server
+app.layout = layout
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True, dev_tools_hot_reload=True, port=8001)
---
+++ website/pages/utils/number/to_decimal.py
@@ -0,0 +1,3 @@
+def to_decimal(value):
+    """Converts a string to a decimal number."""
+    return float(value)
