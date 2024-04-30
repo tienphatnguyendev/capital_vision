2287735680561e03c530d0588b78a3556fc9c1ed
Nguyễn Minh Hoàng <75627141+nguyenhoang2001@users.noreply.github.com>
2024-04-30 20:26:49 +1000

    Add header graphs

---
+++ website/pages/components/header_graph.py
@@ -0,0 +1,80 @@
+import dash
+from dash import dash_table, dcc, callback, Output, Input, html, Patch
+import pandas as pd
+import dash_bootstrap_components as dbc
+import plotly.express as px
+from pages.components.line_graph import line_graph
+from pages.utils.number.format_number import format_number
+import plotly.graph_objs as go
+
+LINE_GRAPH_NAME = ["Revenue", "Gross profit", "Expenses", "Dept Ratio"]
+QUOTATIONS = ["$", "$", "$", ""]
+MOCK_DATA = {"year": [2019, 2020, 2021, 2022], "value": [50, 40, 45, 55]}
+
+graph_style = {"margin-right": "10px", "background-color": "#000000"}
+
+column_elements = []
+for i in range(4):
+    data_frame = pd.DataFrame(MOCK_DATA)
+    column_elements.append(
+        dbc.Col(
+            [
+                line_graph(
+                    data_frame,
+                    f"line-graph-{i}",
+                    f"line-value-{i}",
+                    LINE_GRAPH_NAME[i],
+                ),
+            ]
+        )
+    )
+
+
+def header_container():
+    return dbc.Container(
+        [dbc.Row(column_elements)],
+        style={
+            "height": "100px",
+            "display": "flex",
+            "justify-content": "center",
+            "margin-right": "-100px",
+            "margin-top": "20px",
+        },
+    )
+
+
+for i in range(4):
+
+    @callback(
+        Output(f"line-value-{i}", "children"),
+        Input(f"line-graph-{i}", "clickData"),
+    )
+    def update_line_graph(click_data, i=i):
+        quotation = QUOTATIONS[i]
+        value = click_data["points"][0]["y"] if click_data else MOCK_DATA["value"][0]
+        format_value = format_number(value)
+        return f"{quotation}{format_value}"
+
+    @callback(
+        Output(f"line-graph-{i}", "figure"),
+        [Input(f"line-graph-{i}", "clickData")],
+    )
+    def update_graph(clicked_data, i=i):
+        data_length = len(MOCK_DATA["year"])
+
+        idx = clicked_data["points"][0]["pointIndex"] if clicked_data else 0
+
+        marker_colors = ["grey"] * data_length
+        marker_sizes = [8] * data_length
+        marker_opacity = [0.5] * data_length
+
+        marker_opacity[idx] = 1
+        marker_colors[idx] = "red"
+        marker_sizes[idx] = 12
+
+        patched_figure = Patch()
+        patched_figure["data"][0]["marker"] = dict(
+            color=marker_colors, size=marker_sizes, opacity=marker_opacity
+        )
+        patched_figure["data"][0]["line"] = dict(color="black")
+        return patched_figure
